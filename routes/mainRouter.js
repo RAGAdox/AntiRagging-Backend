2f47f686748037541af918102b4a9a84e309abad
RAGAdox <rishitech882@gmail.com>
2019-03-24 13:26:50 +0530

    JSON CHANGES TO SEE

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -3,6 +3,6 @@ const express = require("express");
 var router = express.Router();
 router.use('/auth', require('./Auth.js'));
 router.get("/",(req,res)=>{
-    res.send("Requested Home");
+    res.json({msg:"Requested Home"});
 })
 module.exports = router;
